757f4f3b5db17cf215fa9e0140e567849e4de718
Allen <378611380@qq.com>
2021-04-25 18:33:43 +0800

    create gulpfile.js and package.json file

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify');
+//Compress js
+gulp.task('compress', function() {
+    return gulp.src(['./src/*.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('./'))
+})
+gulp.task('watch', function() {
+    //Watch .js files
+    gulp.watch('./src/*.js', gulp.series('compress'));
+})
